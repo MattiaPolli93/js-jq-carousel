04dc5ee5e4ce30889f498d802c6c93145e11778a
Mattia <matpolli93@gmail.com>
2021-03-12 16:22:04 +0100

    Buttons changing color at each slide

--- js/functions.js
+++ js/functions.js
@@ -1,25 +1,36 @@
 // Functions
+// Slide to the left (affecting buttons as well)
 function slideLeft() {
     var activeImg = $("img.active");
-    
+    var activeBtn = $(".fa-circle.active");
+
     if (activeImg.hasClass("first")) {
         activeImg.removeClass("active");
-        $(".last").addClass("active");
+        $("img.last").addClass("active");
+        activeBtn.removeClass("active");
+        $("i.last").addClass("active");
     } else {
         activeImg.removeClass("active");
         activeImg.prev().addClass("active");
+        activeBtn.removeClass("active");
+        activeBtn.prev().addClass("active");
     }
 }
 
-// Slide to the right
+// Slide to the right (affecting buttons as well)
 function slideRight() {
     var activeImg = $("img.active");
+    var activeBtn = $(".fa-circle.active");
 
     if (activeImg.hasClass("last")) {
         activeImg.removeClass("active");
-        $(".first").addClass("active");
+        $("img.first").addClass("active");
+        activeBtn.removeClass("active");
+        $("i.first").addClass("active");
     } else {
         activeImg.removeClass("active");
         activeImg.next().addClass("active");
+        activeBtn.removeClass("active");
+        activeBtn.next().addClass("active");
     }
 };
